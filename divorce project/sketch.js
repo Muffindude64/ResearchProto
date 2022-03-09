eafa379614068ffe21b51c93c3e705affd35ad4b
Muffindude64 <97483337+Muffindude64@users.noreply.github.com>
2022-03-08 18:14:06 -0600

    Add files via upload

---
+++ divorce project/sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0, 0, 0);
+
+stroke('white');
+
+strokeWeight(10);
+line(1300, 350, 1500, 0)
+ 
+ line(1300, 350, 1100, 200)
+
+ line(1100, 200, 900, 400)
+
+ line(900, 400, 700, 130)
+
+ line(700, 130, 500, 750)
+
+ line(500, 750, 300, 347)
+
+ line(300, 347, 100, 620)
+
+ line(100, 620, 0, 860)
+}
